f0954dcc42261816762337dedb03ea316c38db49
gilvan <g.r.almeida@live.com>
2019-03-24 01:02:17 -0300

    Adding visibilityFilter reducer

---
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import { SHOW_ALL, SET_VISIBILITY_FILTER } from './actions'
+
+export const initialState = SHOW_ALL
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.payload.filter
+  }
+
+  return state
+}
