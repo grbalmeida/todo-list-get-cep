468734b7dad442a4b84bdec1c4bd208c599dd9de
gilvan <g.r.almeida@live.com>
2019-03-24 13:42:52 -0300

    Adding the createReducer Function

--- src/redux-flow/reducers/visibility-filter/index.js
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -1,14 +1,10 @@
 'use strict'
 
+import createReducer from '../create-reducer'
 import { SHOW_ALL, SET_VISIBILITY_FILTER } from './actions'
 
 export const initialState = SHOW_ALL
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.payload.filter
-  }
-
-  return state
-}
+export default createReducer(initialState, {
+  [SET_VISIBILITY_FILTER]: (state, action) => action.payload.filter
+})
