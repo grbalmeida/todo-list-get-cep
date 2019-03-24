57637b102cdfc5ecff34c6b4040672b740a52b4d
gilvan <g.r.almeida@live.com>
2019-03-24 14:24:20 -0300

    Adding CSS styles

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
+import 'milligram'
 import Form from 'components/form'
 import TodosList from 'components/todos-list'
 import Filter from 'components/filter'
 import SearchCep from 'components/search-cep'
 
 const App = () => (
-  <div>
+  <div style={{ display: 'flex', justifyContent: 'space-around', padding: 30 }}>
     <div>
       <Form />
       <TodosList />
