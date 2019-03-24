54275727af1feac109a16688243959f96a2be2af
gilvan <g.r.almeida@live.com>
2019-03-23 23:36:51 -0300

    Implementation of Todos reducer - Add Todo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 'use strict'
 
 import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from 'reducers/todos/action-creators'
 
-const App = () => (
+const App = ({ todos, handleAddTodo }) => (
   <div>
-    <input type='text' />
+    <form onSubmit={handleAddTodo}>
+      <input type='text' name='todo' />
+      <button type='submit'>Adicionar</button>
+    </form>
     <ul>
       <li style={{ textDecoration: 'line-through' }}>First item</li>
       <li>Second item</li>
@@ -17,4 +22,15 @@ const App = () => (
   </div>
 )
 
-export default App
+const mapStateToProps = (state) => ({
+  todos: state
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  handleAddTodo: (e) => {
+    e.preventDefault()
+    dispatch(addTodo(e.target.todo.value))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
